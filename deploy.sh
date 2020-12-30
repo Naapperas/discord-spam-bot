5b438ce83f7c9948a55c712a12d1ae31905cbd29
Nuno Pereira <nunoafonso2002@gmail.com>
2020-12-30 01:51:20 +0000

    Updated bot!

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 ARG="Updated bot!"
 if [ ! -z "$1" ]; then
-    ARG=$1
+    ARG="$1"
 fi
-bash ./heroku_deploy.sh $ARG
-bash ./github_deploy.sh $ARG
+
+RED='\033[0;31m'
+NC='\033[0m'
+
+echo "${RED} Your Github token is: c223a95bf811b9ddf9d2c42d945d52a1fcfac62b ${NC}"
+
+bash ./heroku_deploy.sh "$ARG"
+bash ./github_deploy.sh "$ARG"
