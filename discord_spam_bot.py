1e97f57b185117ecd480357633a28db51b85f00b
Nuno Pereira <nunoafonso2002@gmail.com>
2020-12-30 00:14:01 +0000

    Setup

--- discord_spam_bot.py
+++ discord_spam_bot.py
@@ -30,7 +30,8 @@ spamming_emotes = {}
 
 emotes = ["kekw", "pepega", "sadge", "peepohappy", "monkaW", "ELIMINAR",
           "pufavo", "pogchamp", "weirdchamp", "homies", "FeelsStrongMan",
-          "5head", "bruh", "caragomeu", "ehehe", "mds", "facho", "caos", "zeapogar"]
+          "5head", "bruh", "caragomeu", "ehehe", "mds", "facho", "caos", "zeapogar", 
+          "poggies"]
 
 
 @bot.event
