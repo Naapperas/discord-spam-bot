1b01ebf95991bc0c90197ea99eb52af6a8341a12
Nuno Pereira <nunoafonso2002@gmail.com>
2021-01-16 19:03:22 +0000

    Updated bot!

--- cogs/spam_bot.py
+++ cogs/spam_bot.py
@@ -2,14 +2,14 @@ from discord.ext import commands
 import discord
 import time
 import random
-# import os
-# import psycopg2
+import os
+import psycopg2
 
-# DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = os.environ['DATABASE_URL']
 
-# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
-# print(conn)
+print(conn)
 
 class Spam_Bot(commands.Cog):
     """
