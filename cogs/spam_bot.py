f3b3196f2a88a1317fa842d0d62519cb8e1dfbd3
Nuno Pereira <nunoafonso2002@gmail.com>
2021-01-28 03:07:17 +0000

    Updated bot!

--- cogs/spam_bot.py
+++ cogs/spam_bot.py
@@ -52,10 +52,10 @@ class Spam_Bot(commands.Cog):
             # Fetch result
             record = cursor.fetchone()
             print("You are connected to - ", record, "\n")
-            cursor.execute("SELECT * from *")
+            cursor.execute("SELECT current_database();")
             # Fetch result
             record = cursor.fetchone()
-            print("You are connected to - ", record, "\n")
+            print("Current database - ", record, "\n")
         except (Exception, Error) as error:
             print(error)
         finally:
